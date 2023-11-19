70a80fa1e731e660f4d899d32c1f0d00d826b6ec
Omi Iyamu <omiiyamu@gmail.com>
2023-11-19 11:58:32 -0800

    Add email analysis functionality

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
 import google.auth
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -13,7 +17,19 @@ from email.mime.text import MIMEText
 import openai
 from openai import OpenAI
 
-client = OpenAI()
+# Assistant Function: High Level Browse
+browse = {
+    "name": "highLevelBrowse",
+    "description": "Browse the web using natural language instructions. This tool enables control over web browsers to fetch and interact with web content.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "instruction": {"type": "string", "description": "Detailed natural language instruction for browsing."},
+            "url": {"type": "string", "description": "Optional starting URL for the browsing session."}
+        },
+        "required": ["instruction"]
+    }
+}
 
 
 # Function to authenticate and create a Gmail API service
@@ -53,22 +69,22 @@ def get_emails(service, user_id='me'):
         return None
     else:
         message = messages[0]
-        msg = service.users().messages().get(userId=user_id, id=message['id'], format='full').execute()
+        full_email = service.users().messages().get(userId=user_id, id=message['id'], format='full').execute()
 
-        headers = msg['payload']['headers']
+        headers = full_email['payload']['headers']
         sender = next(header['value'] for header in headers if header['name'] == 'From')
         subject = next(header['value'] for header in headers if header['name'] == 'Subject')
 
         # Handle different types of email payloads
-        if 'parts' in msg['payload']:
+        if 'parts' in full_email['payload']:
             # Multipart email, could be mixed, alternative, related, etc.
-            part = msg['payload']['parts'][0]
+            part = full_email['payload']['parts'][0]
             body = base64.urlsafe_b64decode(part['body']['data']).decode('utf-8')
         else:
             # Simple email, usually plain text or HTML
-            body = base64.urlsafe_b64decode(msg['payload']['body']['data']).decode('utf-8')
+            body = base64.urlsafe_b64decode(full_email['payload']['body']['data']).decode('utf-8')
 
-        return sender, subject, body
+        return sender, subject, body, full_email
 
 
 # Function to send emails
@@ -92,61 +108,55 @@ def send_email(service, user_id, destination, subject, body):
 
 
 # Function to analyze email with OpenAI API
-def analyze_email(content):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=content,
-      max_tokens=50
-    )
-    return response.choices[0].text.strip()
-
-browse = {
-    "name": "highLevelBrowse",
-    "description": "Browse the web using natural language instructions. This tool enables control over web browsers to fetch and interact with web content.",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "instruction": {"type": "string", "description": "Detailed natural language instruction for browsing."},
-            "url": {"type": "string", "description": "Optional starting URL for the browsing session."}
-        },
-        "required": ["instruction"]
-    }
-}
-
-def agent(query: str):
+def analyze_email(email):
     # Create an Assistant with the MultiOn Browse API
-    assistant = client.beta.assistants.create(
-        instructions="You are an assistant with the capability to browse the web. Use the MultiOn browser to assist users in fetching and interacting with web content.",
-        model="gpt-4-1106-preview",  
-        tools=[{
-            "type": "function",
-            "function": browse
-        }]
-    )
+    # assistant = client.beta.assistants.create(
+    #     instructions="You are an assistant with the capability to browse the web. Use the MultiOn browser to assist users in fetching and interacting with web content.",
+    #     model="gpt-4-1106-preview",  
+    #     tools=[{
+    #         "type": "function",
+    #         "function": browse
+    #     }]
+    # )
+
+    assistant_id = os.getenv("OPENAI_ASSISTANT_ID")
  
     # Create a Thread with the initial user message
-    thread = openai.Thread.create(
-        assistant=assistant.id,
+    thread = openai.beta.threads.create(
         messages=[{
             "role": "user",
-            "content": query
+            "content": email
         }]
     )
  
     # Run the Assistant on the Thread
-    run = openai.Thread.runs.create(
+    run = openai.beta.threads.runs.create(
         thread_id=thread.id,
-        assistant_id=assistant.id
+        assistant_id=assistant_id
+    )
+
+    # Wait for run to complete
+    # success if run.status == "completed"
+    # failure if run.status == "failed", "cancelled", or "expired"
+    # wait 1 second between checks
+    while run.status != "completed":
+        if run.status == "failed" or run.status == "cancelled" or run.status == "expired":
+            return "An error occurred while analyzing the email."
+        time.sleep(1)
+        run = openai.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+    # Retrieve the latest message from the Thread
+    response = openai.beta.threads.messages.list(
+        thread_id=thread.id,
+        limit=1,
+        order='asc',
     )
  
-    # Retrieve the response
-    response = openai.Thread.messages.retrieve(
-        thread_id=thread.id,
-        message_id=run.messages[-1].id
-    )
- 
-    return response.content
+    return response.data[0].content
+
 
 # Main logic
 def main():
@@ -154,19 +164,21 @@ def main():
 
     # Read the most recent email and get its details
     print("Reading the most recent email...")
-    sender, subject, body = get_emails(service)
+    sender, subject, body, full_email = get_emails(service)
     print(f"Sender: {sender}")
     print(f"Subject: {subject}")
 
     # Analyze email content with OpenAI (optional, based on your requirement)
-    # print("Analyzing email content...")
-    # analysis = analyze_email(body)
-    # print(f"Analysis: {analysis}")
+    print("Analyzing email content...")
+    analysis = analyze_email(body)
+    print(f"Analysis: {analysis}")
+
+    # send a text message to the sender with the analysis
 
     # Send a test email (you can replace this with a real recipient and content)
-    print("Sending a test email...")
-    test_email_content = "This is a test email from My Executive Assistant."
-    send_email(service, 'me', 'omiiyamu@gmail.com', 'Test Email from MEA', test_email_content)
+    # print("Sending a test email...")
+    # test_email_content = "This is a test email from My Executive Assistant."
+    # send_email(service, 'me', 'omiiyamu@gmail.com', 'Test Email from MEA', test_email_content)
 
     print("All tests completed.")
 
