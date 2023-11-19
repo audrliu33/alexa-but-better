abb9e3a5770ceaa9132b4949586eeef407c3da27
audrey-liu <audliu@sas.upenn.edu>
2023-11-19 12:34:49 -0800

    basic flask app

--- alexa-but-better/app.py
+++ alexa-but-better/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import join_room, leave_room, send, SocketIO
+from flask import Flask, request
+from agent import *
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET123"
-socketio = SocketIO(app)
 
-if __name__ == "__main__":
-    socketio.run(app, debug=True)
+@app.route('/restaurant')
+def book_restaurant():
+    prompt = request.args.get('prompt')
+    use_multion(prompt)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
